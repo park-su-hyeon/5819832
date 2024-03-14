980f9a210a9c37d2bbfe2fb05fcc4dbd8f355643
park-su-hyeon <162524707+park-su-hyeon@users.noreply.github.com>
2024-03-14 20:05:34 +0900

    Update 240314_5819832_1.c

--- 240314/240314_5819832_1.c
+++ 240314/240314_5819832_1.c
@@ -11,7 +11,7 @@ int main() {
     start = clock(); //시간 측정 시작
 
     for (int i = 2; i <= 100; i++) { 
-        for (n = 2; n <= i; n++) { 
+        for (n = 2; n < i; n++) { 
             if (i % n == 0) //나머지가 0인 수는 소수에서 제외
                 break;
         }
